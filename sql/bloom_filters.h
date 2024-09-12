MariaDB/server

mhnsw: inter-statement shared cache
* preserve the graph in memory between statements
* keep it in a TABLE_SHARE, available for concurrent searches
* nodes are generally read-only, walking the graph doesn't change them
* distance to target is cached, calculated only once
* SIMD-optimized bloom filter detects visited nodes
* nodes are stored in an array, not List, to better utilize bloom filter
* auto-adjusting heuristic to estimate the number of visited nodes
  (to configure the bloom filter)
* many threads can concurrently walk the graph. MEM_ROOT and Hash_set
  are protected with a mutex, but walking doesn't need them
* up to 8 threads can concurrently load nodes into the cache,
  nodes are partitioned into 8 mutexes (8 is chosen arbitrarily, might
  need tuning)
* concurrent editing is not supported though
* this is fine for MyISAM, TL_WRITE protects the TABLE_SHARE and the
  graph (note that TL_WRITE_CONCURRENT_INSERT is not allowed, because an
  INSERT into the main table means multiple UPDATEs in the graph)
* InnoDB uses secondary transaction-level caches linked in a list in
  in thd->ha_data via a fake handlerton
* on rollback the secondary cache is discarded, on commit nodes
  from the secondary cache are invalidated in the shared cache
  while it is exclusively locked
* on savepoint rollback both caches are flushed. this can be improved
  in the future with a row visibility callback
* graph size is controlled by @@mhnsw_cache_size, the cache is flushed
  when it reaches the threshold

sql/bloom_filters.h
@@ -1,0 +1,187 @@
+/*
+MIT License
+
+Copyright (c) 2023 Sasha Krassovsky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// https://save-buffer.github.io/bloom_filter.html
+
+#pragma once
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <immintrin.h>
+
+template <typename T>
+struct PatternedSimdBloomFilter
+{
+  PatternedSimdBloomFilter(int n, float eps) : n(n), epsilon(eps)
+  {
+    m = ComputeNumBits();
+    int log_num_blocks = 32 - __builtin_clz(m) - rotate_bits;
+    num_blocks = (1ULL << log_num_blocks);
+    bv.resize(num_blocks);
+  }
+
+  uint64_t ComputeNumBits()
+  {
+    double bits_per_val = -1.44 * std::log2(epsilon);
+    return std::max<uint64_t>(512, bits_per_val * n + 0.5);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  __m256i CalcHash(__m256i vecData)
+  {
+    // (almost) xxHash parallel version, 64bit input, 64bit output, seed=0
+    static constexpr __m256i rotl48={
+      0x0504030201000706ULL, 0x0D0C0B0A09080F0EULL,
+      0x1514131211101716ULL, 0x1D1C1B1A19181F1EULL
+    };
+    static constexpr __m256i rotl24={
+      0x0201000706050403ULL, 0x0A09080F0E0D0C0BULL,
+      0x1211101716151413ULL, 0x1A19181F1E1D1C1BULL,
+    };
+    static constexpr uint64_t prime_mx2= 0x9FB21C651E98DF25ULL;
+    static constexpr uint64_t bitflip= 0xC73AB174C5ECD5A2ULL;
+    __m256i step1= _mm256_xor_si256(vecData, _mm256_set1_epi64x(bitflip));
+    __m256i step2= _mm256_shuffle_epi8(step1, rotl48);
+    __m256i step3= _mm256_shuffle_epi8(step1, rotl24);
+    __m256i step4= _mm256_xor_si256(step1, _mm256_xor_si256(step2, step3));
+    __m256i step5= _mm256_mul_epi32(step4, _mm256_set1_epi64x(prime_mx2));
+    __m256i step6= _mm256_srli_epi64(step5, 35);
+    __m256i step7= _mm256_add_epi64(step6, _mm256_set1_epi64x(8));
+    __m256i step8= _mm256_xor_si256(step5, step7);
+    __m256i step9= _mm256_mul_epi32(step8, _mm256_set1_epi64x(prime_mx2));
+    return _mm256_xor_si256(step9, _mm256_srli_epi64(step9, 28));
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  __m256i GetBlockIdx(__m256i vecHash)
+  {
+    __m256i vecNumBlocksMask = _mm256_set1_epi64x(num_blocks - 1);
+    __m256i vecBlockIdx = _mm256_srli_epi64(vecHash, mask_idx_bits + rotate_bits);
+    return _mm256_and_si256(vecBlockIdx, vecNumBlocksMask);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  __m256i ConstructMask(__m256i vecHash)
+  {
+    __m256i vecMaskIdxMask = _mm256_set1_epi64x((1 << mask_idx_bits) - 1);
+    __m256i vecMaskMask = _mm256_set1_epi64x((1ull << bits_per_mask) - 1);
+    __m256i vec64 = _mm256_set1_epi64x(64);
+
+    __m256i vecMaskIdx = _mm256_and_si256(vecHash, vecMaskIdxMask);
+    __m256i vecMaskByteIdx = _mm256_srli_epi64(vecMaskIdx, 3);
+    __m256i vecMaskBitIdx = _mm256_and_si256(vecMaskIdx, _mm256_set1_epi64x(0x7));
+    __m256i vecRawMasks = _mm256_i64gather_epi64((const longlong *)masks, vecMaskByteIdx, 1);
+    __m256i vecUnrotated = _mm256_and_si256(_mm256_srlv_epi64(vecRawMasks, vecMaskBitIdx), vecMaskMask);
+
+    __m256i vecRotation = _mm256_and_si256(_mm256_srli_epi64(vecHash, mask_idx_bits), _mm256_set1_epi64x((1 << rotate_bits) - 1));
+    __m256i vecShiftUp = _mm256_sllv_epi64(vecUnrotated, vecRotation);
+    __m256i vecShiftDown = _mm256_srlv_epi64(vecUnrotated, _mm256_sub_epi64(vec64, vecRotation));
+    return _mm256_or_si256(vecShiftDown, vecShiftUp);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  void Insert(const T **data)
+  {
+    __m256i vecDataA = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 0));
+    __m256i vecDataB = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 4));
+
+    __m256i vecHashA= CalcHash(vecDataA);
+    __m256i vecHashB= CalcHash(vecDataB);
+
+    __m256i vecMaskA = ConstructMask(vecHashA);
+    __m256i vecMaskB = ConstructMask(vecHashB);
+
+    __m256i vecBlockIdxA = GetBlockIdx(vecHashA);
+    __m256i vecBlockIdxB = GetBlockIdx(vecHashB);
+
+    uint64_t block0 = _mm256_extract_epi64(vecBlockIdxA, 0);
+    uint64_t block1 = _mm256_extract_epi64(vecBlockIdxA, 1);
+    uint64_t block2 = _mm256_extract_epi64(vecBlockIdxA, 2);
+    uint64_t block3 = _mm256_extract_epi64(vecBlockIdxA, 3);
+    uint64_t block4 = _mm256_extract_epi64(vecBlockIdxB, 0);
+    uint64_t block5 = _mm256_extract_epi64(vecBlockIdxB, 1);
+    uint64_t block6 = _mm256_extract_epi64(vecBlockIdxB, 2);
+    uint64_t block7 = _mm256_extract_epi64(vecBlockIdxB, 3);
+
+    bv[block0] |= _mm256_extract_epi64(vecMaskA, 0);
+    bv[block1] |= _mm256_extract_epi64(vecMaskA, 1);
+    bv[block2] |= _mm256_extract_epi64(vecMaskA, 2);
+    bv[block3] |= _mm256_extract_epi64(vecMaskA, 3);
+    bv[block4] |= _mm256_extract_epi64(vecMaskB, 0);
+    bv[block5] |= _mm256_extract_epi64(vecMaskB, 1);
+    bv[block6] |= _mm256_extract_epi64(vecMaskB, 2);
+    bv[block7] |= _mm256_extract_epi64(vecMaskB, 3);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  uint8_t Query(T **data)
+  {
+    __m256i vecDataA = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 0));
+    __m256i vecDataB = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 4));
+
+    __m256i vecHashA= CalcHash(vecDataA);
+    __m256i vecHashB= CalcHash(vecDataB);
+
+    __m256i vecMaskA = ConstructMask(vecHashA);
+    __m256i vecMaskB = ConstructMask(vecHashB);
+
+    __m256i vecBlockIdxA = GetBlockIdx(vecHashA);
+    __m256i vecBlockIdxB = GetBlockIdx(vecHashB);
+
+    __m256i vecBloomA = _mm256_i64gather_epi64(bv.data(), vecBlockIdxA, sizeof(longlong));
+    __m256i vecBloomB = _mm256_i64gather_epi64(bv.data(), vecBlockIdxB, sizeof(longlong));
+    __m256i vecCmpA = _mm256_cmpeq_epi64(_mm256_and_si256(vecMaskA, vecBloomA), vecMaskA);
+    __m256i vecCmpB = _mm256_cmpeq_epi64(_mm256_and_si256(vecMaskB, vecBloomB), vecMaskB);
+    uint32_t res_a = static_cast<uint32_t>(_mm256_movemask_epi8(vecCmpA));
+    uint32_t res_b = static_cast<uint32_t>(_mm256_movemask_epi8(vecCmpB));
+    uint64_t res_bytes = res_a | (static_cast<uint64_t>(res_b) << 32);
+    uint8_t res_bits = static_cast<uint8_t>(_mm256_movemask_epi8(_mm256_set1_epi64x(res_bytes)) & 0xff);
+    return res_bits;
+  }
+
+  int n;
+  float epsilon;
+
+  uint64_t num_blocks;
+  uint64_t m;
+  // calculated from the upstream MaskTable and hard-coded
+  static constexpr int log_num_masks = 10;
+  static constexpr int bits_per_mask = 57;
+  const uint8_t masks[136]= {0x00, 0x04, 0x01, 0x04, 0x00, 0x20, 0x01, 0x00,
+    0x00, 0x02, 0x08, 0x00, 0x02, 0x42, 0x00, 0x00, 0x04, 0x00, 0x00, 0x84,
+    0x80, 0x00, 0x04, 0x00, 0x02, 0x00, 0x00, 0x21, 0x00, 0x08, 0x00, 0x14,
+    0x00, 0x00, 0x40, 0x00, 0x10, 0x00, 0xa8, 0x00, 0x00, 0x00, 0x00, 0x10,
+    0x04, 0x40, 0x01, 0x00, 0x40, 0x00, 0x00, 0x08, 0x01, 0x02, 0x80, 0x00,
+    0x00, 0x01, 0x00, 0x06, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x0c, 0x10,
+    0x00, 0x10, 0x00, 0x00, 0x10, 0x08, 0x01, 0x10, 0x00, 0x00, 0x10, 0x20,
+    0x00, 0x01, 0x20, 0x00, 0x02, 0x40, 0x00, 0x00, 0x02, 0x40, 0x01, 0x00,
+    0x40, 0x00, 0x00, 0x0a, 0x00, 0x02, 0x01, 0x80, 0x00, 0x00, 0x10, 0x08,
+    0x00, 0x06, 0x00, 0x04, 0x00, 0x00, 0x50, 0x00, 0x08, 0x10, 0x20, 0x00,
+    0x00, 0x80, 0x00, 0x10, 0x10, 0x04, 0x04, 0x00, 0x00, 0x00, 0x20, 0x20,
+    0x08, 0x08, 0x02, 0x00, 0x00, 0x00, 0x40, 0x00};
+  std::vector<longlong> bv;
+
+  static constexpr int mask_idx_bits = log_num_masks;
+  static constexpr int rotate_bits = 6;
+};

user0x1337/mariadb-server

sql/bloom_filters.h
@@ -1,0 +1,191 @@
+/*
+MIT License
+
+Copyright (c) 2023 Sasha Krassovsky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// https://save-buffer.github.io/bloom_filter.html
+
+#pragma once
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#ifdef HAVE_IMMINTRIN_H
+#include <immintrin.h>
+#endif
+
+template <typename T>
+struct PatternedSimdBloomFilter
+{
+  PatternedSimdBloomFilter(int n, float eps) : n(n), epsilon(eps)
+  {
+    m = ComputeNumBits();
+    int log_num_blocks = my_bit_log2_uint32(m) + 1 - rotate_bits;
+    num_blocks = (1ULL << log_num_blocks);
+    bv.resize(num_blocks);
+  }
+
+  uint32_t ComputeNumBits()
+  {
+    double bits_per_val = -1.44 * std::log2(epsilon);
+    return std::max<uint32_t>(512, static_cast<uint32_t>(bits_per_val * n + 0.5));
+  }
+
+#if __GNUC__ > 7 && defined(HAVE_IMMINTRIN_H)
+  __attribute__ ((target ("avx2,avx,fma")))
+  __m256i CalcHash(__m256i vecData)
+  {
+    // (almost) xxHash parallel version, 64bit input, 64bit output, seed=0
+    static constexpr __m256i rotl48={
+      0x0504030201000706ULL, 0x0D0C0B0A09080F0EULL,
+      0x1514131211101716ULL, 0x1D1C1B1A19181F1EULL
+    };
+    static constexpr __m256i rotl24={
+      0x0201000706050403ULL, 0x0A09080F0E0D0C0BULL,
+      0x1211101716151413ULL, 0x1A19181F1E1D1C1BULL,
+    };
+    static constexpr uint64_t prime_mx2= 0x9FB21C651E98DF25ULL;
+    static constexpr uint64_t bitflip= 0xC73AB174C5ECD5A2ULL;
+    __m256i step1= _mm256_xor_si256(vecData, _mm256_set1_epi64x(bitflip));
+    __m256i step2= _mm256_shuffle_epi8(step1, rotl48);
+    __m256i step3= _mm256_shuffle_epi8(step1, rotl24);
+    __m256i step4= _mm256_xor_si256(step1, _mm256_xor_si256(step2, step3));
+    __m256i step5= _mm256_mul_epi32(step4, _mm256_set1_epi64x(prime_mx2));
+    __m256i step6= _mm256_srli_epi64(step5, 35);
+    __m256i step7= _mm256_add_epi64(step6, _mm256_set1_epi64x(8));
+    __m256i step8= _mm256_xor_si256(step5, step7);
+    __m256i step9= _mm256_mul_epi32(step8, _mm256_set1_epi64x(prime_mx2));
+    return _mm256_xor_si256(step9, _mm256_srli_epi64(step9, 28));
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  __m256i GetBlockIdx(__m256i vecHash)
+  {
+    __m256i vecNumBlocksMask = _mm256_set1_epi64x(num_blocks - 1);
+    __m256i vecBlockIdx = _mm256_srli_epi64(vecHash, mask_idx_bits + rotate_bits);
+    return _mm256_and_si256(vecBlockIdx, vecNumBlocksMask);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  __m256i ConstructMask(__m256i vecHash)
+  {
+    __m256i vecMaskIdxMask = _mm256_set1_epi64x((1 << mask_idx_bits) - 1);
+    __m256i vecMaskMask = _mm256_set1_epi64x((1ull << bits_per_mask) - 1);
+    __m256i vec64 = _mm256_set1_epi64x(64);
+
+    __m256i vecMaskIdx = _mm256_and_si256(vecHash, vecMaskIdxMask);
+    __m256i vecMaskByteIdx = _mm256_srli_epi64(vecMaskIdx, 3);
+    __m256i vecMaskBitIdx = _mm256_and_si256(vecMaskIdx, _mm256_set1_epi64x(0x7));
+    __m256i vecRawMasks = _mm256_i64gather_epi64((const longlong *)masks, vecMaskByteIdx, 1);
+    __m256i vecUnrotated = _mm256_and_si256(_mm256_srlv_epi64(vecRawMasks, vecMaskBitIdx), vecMaskMask);
+
+    __m256i vecRotation = _mm256_and_si256(_mm256_srli_epi64(vecHash, mask_idx_bits), _mm256_set1_epi64x((1 << rotate_bits) - 1));
+    __m256i vecShiftUp = _mm256_sllv_epi64(vecUnrotated, vecRotation);
+    __m256i vecShiftDown = _mm256_srlv_epi64(vecUnrotated, _mm256_sub_epi64(vec64, vecRotation));
+    return _mm256_or_si256(vecShiftDown, vecShiftUp);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  void Insert(const T **data)
+  {
+    __m256i vecDataA = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 0));
+    __m256i vecDataB = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 4));
+
+    __m256i vecHashA= CalcHash(vecDataA);
+    __m256i vecHashB= CalcHash(vecDataB);
+
+    __m256i vecMaskA = ConstructMask(vecHashA);
+    __m256i vecMaskB = ConstructMask(vecHashB);
+
+    __m256i vecBlockIdxA = GetBlockIdx(vecHashA);
+    __m256i vecBlockIdxB = GetBlockIdx(vecHashB);
+
+    uint64_t block0 = _mm256_extract_epi64(vecBlockIdxA, 0);
+    uint64_t block1 = _mm256_extract_epi64(vecBlockIdxA, 1);
+    uint64_t block2 = _mm256_extract_epi64(vecBlockIdxA, 2);
+    uint64_t block3 = _mm256_extract_epi64(vecBlockIdxA, 3);
+    uint64_t block4 = _mm256_extract_epi64(vecBlockIdxB, 0);
+    uint64_t block5 = _mm256_extract_epi64(vecBlockIdxB, 1);
+    uint64_t block6 = _mm256_extract_epi64(vecBlockIdxB, 2);
+    uint64_t block7 = _mm256_extract_epi64(vecBlockIdxB, 3);
+
+    bv[block0] |= _mm256_extract_epi64(vecMaskA, 0);
+    bv[block1] |= _mm256_extract_epi64(vecMaskA, 1);
+    bv[block2] |= _mm256_extract_epi64(vecMaskA, 2);
+    bv[block3] |= _mm256_extract_epi64(vecMaskA, 3);
+    bv[block4] |= _mm256_extract_epi64(vecMaskB, 0);
+    bv[block5] |= _mm256_extract_epi64(vecMaskB, 1);
+    bv[block6] |= _mm256_extract_epi64(vecMaskB, 2);
+    bv[block7] |= _mm256_extract_epi64(vecMaskB, 3);
+  }
+
+  __attribute__ ((target ("avx2,avx,fma")))
+  uint8_t Query(T **data)
+  {
+    __m256i vecDataA = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 0));
+    __m256i vecDataB = _mm256_loadu_si256(reinterpret_cast<__m256i *>(data + 4));
+
+    __m256i vecHashA= CalcHash(vecDataA);
+    __m256i vecHashB= CalcHash(vecDataB);
+
+    __m256i vecMaskA = ConstructMask(vecHashA);
+    __m256i vecMaskB = ConstructMask(vecHashB);
+
+    __m256i vecBlockIdxA = GetBlockIdx(vecHashA);
+    __m256i vecBlockIdxB = GetBlockIdx(vecHashB);
+
+    __m256i vecBloomA = _mm256_i64gather_epi64(bv.data(), vecBlockIdxA, sizeof(longlong));
+    __m256i vecBloomB = _mm256_i64gather_epi64(bv.data(), vecBlockIdxB, sizeof(longlong));
+    __m256i vecCmpA = _mm256_cmpeq_epi64(_mm256_and_si256(vecMaskA, vecBloomA), vecMaskA);
+    __m256i vecCmpB = _mm256_cmpeq_epi64(_mm256_and_si256(vecMaskB, vecBloomB), vecMaskB);
+    uint32_t res_a = static_cast<uint32_t>(_mm256_movemask_epi8(vecCmpA));
+    uint32_t res_b = static_cast<uint32_t>(_mm256_movemask_epi8(vecCmpB));
+    uint64_t res_bytes = res_a | (static_cast<uint64_t>(res_b) << 32);
+    uint8_t res_bits = static_cast<uint8_t>(_mm256_movemask_epi8(_mm256_set1_epi64x(res_bytes)) & 0xff);
+    return res_bits;
+  }
+#endif
+
+  int n;
+  float epsilon;
+
+  uint64_t num_blocks;
+  uint32_t m;
+  // calculated from the upstream MaskTable and hard-coded
+  static constexpr int log_num_masks = 10;
+  static constexpr int bits_per_mask = 57;
+  const uint8_t masks[136]= {0x00, 0x04, 0x01, 0x04, 0x00, 0x20, 0x01, 0x00,
+    0x00, 0x02, 0x08, 0x00, 0x02, 0x42, 0x00, 0x00, 0x04, 0x00, 0x00, 0x84,
+    0x80, 0x00, 0x04, 0x00, 0x02, 0x00, 0x00, 0x21, 0x00, 0x08, 0x00, 0x14,
+    0x00, 0x00, 0x40, 0x00, 0x10, 0x00, 0xa8, 0x00, 0x00, 0x00, 0x00, 0x10,
+    0x04, 0x40, 0x01, 0x00, 0x40, 0x00, 0x00, 0x08, 0x01, 0x02, 0x80, 0x00,
+    0x00, 0x01, 0x00, 0x06, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x0c, 0x10,
+    0x00, 0x10, 0x00, 0x00, 0x10, 0x08, 0x01, 0x10, 0x00, 0x00, 0x10, 0x20,
+    0x00, 0x01, 0x20, 0x00, 0x02, 0x40, 0x00, 0x00, 0x02, 0x40, 0x01, 0x00,
+    0x40, 0x00, 0x00, 0x0a, 0x00, 0x02, 0x01, 0x80, 0x00, 0x00, 0x10, 0x08,
+    0x00, 0x06, 0x00, 0x04, 0x00, 0x00, 0x50, 0x00, 0x08, 0x10, 0x20, 0x00,
+    0x00, 0x80, 0x00, 0x10, 0x10, 0x04, 0x04, 0x00, 0x00, 0x00, 0x20, 0x20,
+    0x08, 0x08, 0x02, 0x00, 0x00, 0x00, 0x40, 0x00};
+  std::vector<longlong> bv;
+
+  static constexpr int mask_idx_bits = log_num_masks;
+  static constexpr int rotate_bits = 6;
+};