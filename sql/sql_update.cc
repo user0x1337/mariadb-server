9162fbe4a619f2b7e7857bf50af4291e2365d25b
Dmitry Shulga <dmitry.shulga@mariadb.com>
2024-09-13 16:13:34 +0700

    MDEV-34551: Column list in the trigger definition
    Added support of the clause `UPDATE OF <columns>` for
    BEFORE/AFTER UPDATE triggers. Triggers defined with this clause
    are fired and run actions only in case an UPDATE statement affects
    any of the listed columns. For columns not specified in the clause
    `UPDATE OF <columns>`, an UPDATE statement with such columns as
    targets don't result in running a trigger.
    
    Output of SHOW TRIGGERS isn't affected by this task. Output of
    the statement SHOW CREATE TRIGGER shows the clause `UPDATE OF <columns>`
    if it was specified on trigger creation.
    
    Tests accompany this task don't include tests that checking for cooperation of
    the statement LOAD DATA and the clause `UPDATE OF <columns>` for
    BEFORE/AFTER UPDATE triggers since the statement LOAD DATA is treated like
    the statement INSERT INTO and therefore doesn't fire BEFORE/AFTER UPDATE
    triggers.

--- sql/sql_update.cc
+++ sql/sql_update.cc
@@ -1068,7 +1068,8 @@ error:
 
       if (table->triggers &&
           unlikely(table->triggers->process_triggers(thd, TRG_EVENT_UPDATE,
-                                                     TRG_ACTION_AFTER, TRUE)))
+                                                     TRG_ACTION_AFTER, true,
+                                                     fields)))
       {
         error= 1;
         thd->bulk_param= save_bulk_param;
@@ -2392,7 +2393,8 @@ int multi_update::send_data(List<Item> &not_used_values)
       }
       if (table->triggers &&
           unlikely(table->triggers->process_triggers(thd, TRG_EVENT_UPDATE,
-                                                     TRG_ACTION_AFTER, TRUE)))
+                                                     TRG_ACTION_AFTER, true,
+                                                     fields_for_table[offset])))
         DBUG_RETURN(1);
     }
     else
@@ -2652,7 +2654,8 @@ int multi_update::do_updates()
         goto err2;
       if (table->triggers &&
           table->triggers->process_triggers(thd, TRG_EVENT_UPDATE,
-                                            TRG_ACTION_BEFORE, TRUE))
+                                            TRG_ACTION_BEFORE, true,
+                                            fields_for_table[offset]))
         goto err2;
 
       if (!can_compare_record || compare_record(table))
@@ -2712,7 +2715,8 @@ int multi_update::do_updates()
 
       if (table->triggers &&
           unlikely(table->triggers->process_triggers(thd, TRG_EVENT_UPDATE,
-                                                     TRG_ACTION_AFTER, TRUE)))
+                                                     TRG_ACTION_AFTER, true,
+                                                     fields_for_table[offset])))
         goto err2;
     }
 
