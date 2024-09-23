MariaDB/server

initial support for vector indexes
MDEV-33407 Parser support for vector indexes

The syntax is

  create table t1 (... vector index (v) ...);

limitation:
* v is a binary string and NOT NULL
* only one vector index per table
* temporary tables are not supported

MDEV-33404 Engine-independent indexes: subtable method

added support for so-called "high level indexes", they are not visible
to the storage engine, implemented on the sql level. For every such
an index in a table, say, t1, the server implicitly creates a second
table named, like, t1#i#05 (where "05" is the index number in t1).
This table has a fixed structure, no frm, not accessible directly,
doesn't go into the table cache, needs no MDLs.

MDEV-33406 basic optimizer support for k-NN searches

for a query like SELECT ... ORDER BY func() optimizer will use
item_func->part_of_sortkey() to decide what keys can be used
to resolve ORDER BY.

sql/handler.cc
@@ -48,6 +48,7 @@
 #include "rowid_filter.h"
 #include "mysys_err.h"
 #include "optimizer_defaults.h"
+#include "vector_mhnsw.h"
 
 #ifdef WITH_PARTITION_STORAGE_ENGINE
 #include "ha_partition.h"
@@ -3494,7 +3495,7 @@ PSI_table_share *handler::ha_table_share_psi() const
 const char *handler::index_type(uint key_number)
 {
   static const char* alg2str[]= { "???", "BTREE", "SPATIAL", "HASH",
-                                  "FULLTEXT", "HASH", "HASH" };
+                                  "FULLTEXT", "HASH", "HASH", "VECTOR" };
   enum ha_key_alg alg= table_share->key_info[key_number].algorithm;
   if (!alg)
   {
@@ -6204,7 +6205,37 @@ int ha_create_table(THD *thd, const char *path, const char *db,
       goto err;
   }
 
-  error= ha_create_table_from_share(thd, &share, create_info);
+  if ((error= ha_create_table_from_share(thd, &share, create_info)))
+    goto err;
+
+  /* create secondary tables for high level indexes */
+  if (share.total_keys > share.keys)
+  {
+    /* as of now: only one vector index can be here */
+    DBUG_ASSERT(share.total_keys == share.keys + 1);
+    DBUG_ASSERT(share.key_info[share.keys].algorithm == HA_KEY_ALG_MHNSW);
+    TABLE_SHARE index_share;
+    char file_name[FN_REFLEN+1];
+    HA_CREATE_INFO index_cinfo;
+    char *path_end= strmov(file_name, path);
+
+    if ((error= share.path.length > sizeof(file_name) - HLINDEX_BUF_LEN))
+      goto err;
+
+    for (uint i= share.keys; i < share.total_keys; i++)
+    {
+      my_snprintf(path_end, HLINDEX_BUF_LEN, HLINDEX_TEMPLATE, i);
+      init_tmp_table_share(thd, &index_share, db, 0, table_name, file_name);
+      index_share.db_plugin= share.db_plugin;
+      if ((error= index_share.init_from_sql_statement_string(thd, false,
+                        mhnsw_hlindex_table.str, mhnsw_hlindex_table.length)))
+        break;
+
+      if ((error= ha_create_table_from_share(thd, &index_share, &index_cinfo)))
+        break;
+    }
+    free_table_share(&index_share);
+  }
 
 err:
   free_table_share(&share);
@@ -7452,6 +7483,8 @@ int handler::ha_reset()
     delete lookup_handler;
     lookup_handler= this;
   }
+  if (table->reset_hlindexes())
+    return 1;
   DBUG_RETURN(reset());
 }
 
@@ -7845,8 +7878,12 @@ bool handler::prepare_for_row_logging()
 
 int handler::prepare_for_insert(bool do_create)
 {
+  if (table->open_hlindexes_for_write())
+    return 1;
+
   /* Preparation for unique of blob's */
-  if (table->s->long_unique_table || table->s->period.unique_keys)
+  if (table->s->long_unique_table || table->s->period.unique_keys ||
+      table->hlindex)
   {
     if (do_create && create_lookup_handler())
       return 1;
@@ -7887,7 +7924,7 @@ int handler::ha_write_row(const uchar *buf)
                       { error= write_row(buf); })
 
   MYSQL_INSERT_ROW_DONE(error);
-  if (likely(!error))
+  if (!error && !((error= table->update_hlindexes())))
   {
     rows_changed++;
     Log_func *log_func= Write_rows_log_event::binlog_row_logging_function;

sql/item_vectorfunc.cc
@@ -24,6 +24,19 @@
 #include <my_global.h>
 #include "item.h"
 
+key_map Item_func_vec_distance::part_of_sortkey() const
+{
+  key_map map(0);
+  if (Item_field *item= get_field_arg())
+  {
+    Field *f= item->field;
+    for (uint i= f->table->s->keys; i < f->table->s->total_keys; i++)
+      if (f->table->s->key_info[i].algorithm == HA_KEY_ALG_MHNSW &&
+          f->key_start.is_set(i))
+        map.set_bit(i);
+  }
+  return map;
+}
 
 double Item_func_vec_distance::val_real()
 {

sql/sql_handler.cc
@@ -669,6 +669,7 @@ mysql_ha_fix_cond_and_key(SQL_HANDLER *handler,
 
       if (c_key->algorithm == HA_KEY_ALG_RTREE ||
           c_key->algorithm == HA_KEY_ALG_FULLTEXT ||
+          c_key->algorithm == HA_KEY_ALG_MHNSW ||
           (ha_rkey_mode != HA_READ_KEY_EXACT &&
            (table->key_info[handler->keyno].index_flags &
             (HA_READ_NEXT | HA_READ_PREV | HA_READ_RANGE)) == 0))

sql/sql_show.cc
@@ -2337,7 +2337,7 @@ int show_create_table_ex(THD *thd, TABLE_LIST *table_list,
   key_info= table->s->key_info;
   primary_key= share->primary_key;
 
-  for (uint i=0 ; i < share->keys ; i++,key_info++)
+  for (uint i=0 ; i < share->total_keys ; i++,key_info++)
   {
     if (key_info->flags & HA_INVISIBLE_KEY)
       continue;
@@ -2360,6 +2360,8 @@ int show_create_table_ex(THD *thd, TABLE_LIST *table_list,
       packet->append(STRING_WITH_LEN("FULLTEXT KEY "));
     else if (key_info->algorithm == HA_KEY_ALG_RTREE)
       packet->append(STRING_WITH_LEN("SPATIAL KEY "));
+    else if (key_info->algorithm == HA_KEY_ALG_MHNSW)
+      packet->append(STRING_WITH_LEN("VECTOR KEY "));
     else
       packet->append(STRING_WITH_LEN("KEY "));
 
@@ -2385,6 +2387,7 @@ int show_create_table_ex(THD *thd, TABLE_LIST *table_list,
         append_identifier(thd, packet, &key_part->field->field_name);
       if (key_part->field &&
           key_part->length != table->field[key_part->fieldnr-1]->key_length() &&
+          key_info->algorithm != HA_KEY_ALG_MHNSW &&
           key_info->algorithm != HA_KEY_ALG_RTREE &&
           key_info->algorithm != HA_KEY_ALG_FULLTEXT)
       {
@@ -7072,7 +7075,7 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
     }
 #endif
 
-    for (uint i=0 ; i < show_table->s->keys ; i++,key_info++)
+    for (uint i=0 ; i < show_table->s->total_keys ; i++,key_info++)
     {
       if ((key_info->flags & HA_INVISIBLE_KEY) &&
           !DBUG_IF("test_invisible_index"))
@@ -7130,6 +7133,7 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
                                    ? "D" : "A", 1, cs);
             table->field[8]->set_notnull();
           }
+          if (i < show_table->s->keys)
           {
             /*
               We have to use table key information to get the key statistics
@@ -7147,6 +7151,12 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
             const char *tmp= show_table->file->index_type(i);
             table->field[13]->store(tmp, strlen(tmp), cs);
           }
+          else
+          {
+            /* there are no others at the moment */
+            DBUG_ASSERT(key_info->algorithm == HA_KEY_ALG_MHNSW);
+            table->field[13]->store(STRING_WITH_LEN("VECTOR"), cs);
+          }
         }
         if (key_info->algorithm != HA_KEY_ALG_FULLTEXT &&
             (key_part->field &&
@@ -7160,7 +7170,7 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
         uint flags= key_part->field ? key_part->field->flags : 0;
         const char *pos= flags & NOT_NULL_FLAG ? "" : "YES";
         table->field[12]->store(pos, strlen(pos), cs);
-        if (!show_table->s->keys_in_use.is_set(i))
+        if (i < show_table->s->keys && !show_table->s->keys_in_use.is_set(i))
           table->field[14]->store(STRING_WITH_LEN("disabled"), cs);
         DBUG_ASSERT(MY_TEST(key_info->flags & HA_USES_COMMENT) ==
                    (key_info->comment.length > 0));

sql/vector_mhnsw.cc
@@ -1,0 +1,209 @@
+/*
+   Copyright (c) 2024, MariaDB plc
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1335  USA
+*/
+
+#include <my_global.h>
+#include "vector_mhnsw.h"
+#include "field.h"
+#include "item.h"
+#include "sql_queue.h"
+#include <scope.h>
+
+const LEX_CSTRING mhnsw_hlindex_table={STRING_WITH_LEN("\
+  CREATE TABLE i (                                      \
+    src varbinary(255) not null,                        \
+    dst varbinary(255) not null,                        \
+    index (src))                                        \
+")};
+
+static void store_ref(TABLE *t, handler *h, uint n)
+{
+  t->hlindex->field[n]->store((char*)h->ref, h->ref_length, &my_charset_bin);
+}
+
+int mhnsw_insert(TABLE *table, KEY *keyinfo)
+{
+  TABLE *graph= table->hlindex;
+  MY_BITMAP *old_map= dbug_tmp_use_all_columns(table, &table->read_set);
+  Field *field= keyinfo->key_part->field;
+  String buf, *res= field->val_str(&buf);
+  handler *h= table->file;
+  int err= 0;
+  dbug_tmp_restore_column_map(&table->read_set, old_map);
+
+  /* metadata are checked on open */
+  DBUG_ASSERT(graph);
+  DBUG_ASSERT(keyinfo->algorithm == HA_KEY_ALG_MHNSW);
+  DBUG_ASSERT(keyinfo->usable_key_parts == 1);
+  DBUG_ASSERT(field->binary());
+  DBUG_ASSERT(field->cmp_type() == STRING_RESULT);
+  DBUG_ASSERT(res); // ER_INDEX_CANNOT_HAVE_NULL
+  DBUG_ASSERT(h->ref_length <= graph->field[0]->field_length);
+  DBUG_ASSERT(h->ref_length <= graph->field[1]->field_length);
+
+  if (res->length() == 0 || res->length() % 4)
+    return 1;
+
+  // let's do every node to every node
+  h->position(table->record[0]);
+  graph->field[0]->store(1);
+  store_ref(table, h, 0);
+
+  if (h->lookup_handler->ha_rnd_init(1))
+    return 1;
+  while (! ((err= h->lookup_handler->ha_rnd_next(h->lookup_buffer))))
+  {
+    h->lookup_handler->position(h->lookup_buffer);
+    if (graph->field[0]->cmp(h->lookup_handler->ref) == 0)
+      continue;
+    store_ref(table, h->lookup_handler, 1);
+    if ((err= graph->file->ha_write_row(graph->record[0])))
+      break;
+  }
+  h->lookup_handler->ha_rnd_end();
+
+  return err == HA_ERR_END_OF_FILE ? 0 : err;
+}
+
+static int cmp_float(void *, float *a, float *b)
+{
+  return *a < *b ? -1 : *a == *b ? 0 : 1;
+}
+
+struct Node
+{
+  float distance;
+  uchar ref[1000];
+};
+
+int mhnsw_first(TABLE *table, Item *dist, ulonglong limit)
+{
+  TABLE *graph= table->hlindex;
+  Queue<Node, float> todo, result;
+  Node *cur;
+  String *str, strbuf;
+  const size_t ref_length= table->file->ref_length;
+  const size_t element_size= ref_length + sizeof(float);
+  uchar *key= (uchar*)alloca(ref_length + 32);
+  Hash_set<Node> visited(PSI_INSTRUMENT_MEM, &my_charset_bin, limit,
+                         sizeof(float), ref_length, 0, 0, HASH_UNIQUE);
+  uint keylen;
+  int err= 0;
+
+  DBUG_ASSERT(graph);
+
+  if (todo.init(1000, 0, 0, cmp_float)) // XXX + autoextent
+    return HA_ERR_OUT_OF_MEM;
+
+  if (result.init(limit, 0, 1, cmp_float))
+    return HA_ERR_OUT_OF_MEM;
+
+  if ((err= graph->file->ha_index_init(0, 1)))
+    return err;
+
+  SCOPE_EXIT([graph](){ graph->file->ha_index_end(); });
+
+  // 1. read a start row
+  if ((err= graph->file->ha_index_last(graph->record[0])))
+    return err;
+
+  if (!(str= graph->field[0]->val_str(&strbuf)))
+    return HA_ERR_CRASHED;
+
+  DBUG_ASSERT(str->length() == ref_length);
+
+  cur= (Node*)table->in_use->alloc(element_size);
+  memcpy(cur->ref, str->ptr(), ref_length);
+
+  if ((err= table->file->ha_rnd_init(0)))
+    return err;
+
+  if ((err= table->file->ha_rnd_pos(table->record[0], cur->ref)))
+    return HA_ERR_CRASHED;
+
+  // 2. add it to the todo
+  cur->distance= dist->val_real();
+  if (dist->is_null())
+    return HA_ERR_END_OF_FILE;
+  todo.push(cur);
+  visited.insert(cur);
+
+  while (todo.elements())
+  {
+    // 3. pick the top node from the todo
+    cur= todo.pop();
+
+    // 4. add it to the result
+    if (result.is_full())
+    {
+      // 5. if not added, greedy search done
+      if (cur->distance > result.top()->distance)
+        break;
+      result.replace_top(cur);
+    }
+    else
+      result.push(cur);
+
+    float threshold= result.is_full() ? result.top()->distance : FLT_MAX;
+
+    // 6. add all its [yet unvisited] neighbours to the todo heap
+    keylen= graph->field[0]->get_key_image(key, ref_length, Field::itRAW);
+    if ((err= graph->file->ha_index_read_map(graph->record[0], key, 3,
+                                             HA_READ_KEY_EXACT)))
+      return HA_ERR_CRASHED;
+
+    do {
+      if (!(str= graph->field[1]->val_str(&strbuf)))
+        return HA_ERR_CRASHED;
+
+      if (visited.find(str->ptr(), ref_length))
+        continue;
+
+      if ((err= table->file->ha_rnd_pos(table->record[0], (uchar*)str->ptr())))
+        return HA_ERR_CRASHED;
+
+      float distance= dist->val_real();
+      if (distance > threshold)
+        continue;
+
+      cur= (Node*)table->in_use->alloc(element_size);
+      cur->distance= distance;
+      memcpy(cur->ref, str->ptr(), ref_length);
+      todo.push(cur);
+      visited.insert(cur);
+    } while (!graph->file->ha_index_next_same(graph->record[0], key, keylen));
+    // 7. goto 3
+  }
+
+  // 8. return results
+  Node **context= (Node**)table->in_use->alloc(sizeof(Node**)*result.elements()+1);
+  graph->context= context;
+
+  Node **ptr= context+result.elements();
+  *ptr= 0;
+  while (result.elements())
+    *--ptr= result.pop();
+
+  return mhnsw_next(table);
+}
+
+int mhnsw_next(TABLE *table)
+{
+  Node ***context= (Node***)&table->hlindex->context;
+  if (**context)
+    return table->file->ha_rnd_pos(table->record[0], (*(*context)++)->ref);
+  return HA_ERR_END_OF_FILE;
+}

user0x1337/mariadb-server

sql/handler.cc
@@ -48,6 +48,7 @@
 #include "rowid_filter.h"
 #include "mysys_err.h"
 #include "optimizer_defaults.h"
+#include "vector_mhnsw.h"
 
 #ifdef WITH_PARTITION_STORAGE_ENGINE
 #include "ha_partition.h"
@@ -3574,7 +3575,7 @@ PSI_table_share *handler::ha_table_share_psi() const
 const char *handler::index_type(uint key_number)
 {
   static const char* alg2str[]= { "???", "BTREE", "SPATIAL", "HASH",
-                                  "FULLTEXT", "HASH", "HASH" };
+                                  "FULLTEXT", "HASH", "HASH", "VECTOR" };
   enum ha_key_alg alg= table_share->key_info[key_number].algorithm;
   if (!alg)
   {
@@ -6398,7 +6399,37 @@ int ha_create_table(THD *thd, const char *path, const char *db,
       goto err;
   }
 
-  error= ha_create_table_from_share(thd, &share, create_info);
+  if ((error= ha_create_table_from_share(thd, &share, create_info)))
+    goto err;
+
+  /* create secondary tables for high level indexes */
+  if (share.total_keys > share.keys)
+  {
+    /* as of now: only one vector index can be here */
+    DBUG_ASSERT(share.total_keys == share.keys + 1);
+    DBUG_ASSERT(share.key_info[share.keys].algorithm == HA_KEY_ALG_VECTOR);
+    TABLE_SHARE index_share;
+    char file_name[FN_REFLEN+1];
+    HA_CREATE_INFO index_cinfo;
+    char *path_end= strmov(file_name, path);
+
+    if ((error= share.path.length > sizeof(file_name) - HLINDEX_BUF_LEN))
+      goto err;
+
+    for (uint i= share.keys; i < share.total_keys; i++)
+    {
+      my_snprintf(path_end, HLINDEX_BUF_LEN, HLINDEX_TEMPLATE, i);
+      init_tmp_table_share(thd, &index_share, db, 0, table_name, file_name, 1);
+      index_share.db_plugin= share.db_plugin;
+      if ((error= index_share.init_from_sql_statement_string(thd, false,
+                        mhnsw_hlindex_table.str, mhnsw_hlindex_table.length)))
+        break;
+
+      if ((error= ha_create_table_from_share(thd, &index_share, &index_cinfo)))
+        break;
+    }
+    free_table_share(&index_share);
+  }
 
 err:
   free_table_share(&share);
@@ -7648,6 +7679,8 @@ int handler::ha_reset()
     delete lookup_handler;
     lookup_handler= this;
   }
+  if (table->reset_hlindexes())
+    return 1;
   DBUG_RETURN(reset());
 }
 
@@ -8039,8 +8072,12 @@ bool handler::prepare_for_row_logging()
 
 int handler::prepare_for_insert(bool do_create)
 {
+  if (table->open_hlindexes_for_write())
+    return 1;
+
   /* Preparation for unique of blob's */
-  if (table->s->long_unique_table || table->s->period.unique_keys)
+  if (table->s->long_unique_table || table->s->period.unique_keys ||
+      table->hlindex)
   {
     if (do_create && create_lookup_handler())
       return 1;
@@ -8090,7 +8127,7 @@ int handler::ha_write_row(const uchar *buf)
                       { error= write_row(buf); })
 
   MYSQL_INSERT_ROW_DONE(error);
-  if (likely(!error))
+  if (!error && !((error= table->update_hlindexes())))
   {
     rows_stats.inserted++;
     Log_func *log_func= Write_rows_log_event::binlog_row_logging_function;

sql/item_vectorfunc.cc
@@ -24,6 +24,19 @@
 #include <my_global.h>
 #include "item.h"
 
+key_map Item_func_vec_distance::part_of_sortkey() const
+{
+  key_map map(0);
+  if (Item_field *item= get_field_arg())
+  {
+    Field *f= item->field;
+    for (uint i= f->table->s->keys; i < f->table->s->total_keys; i++)
+      if (f->table->s->key_info[i].algorithm == HA_KEY_ALG_VECTOR &&
+          f->key_start.is_set(i))
+        map.set_bit(i);
+  }
+  return map;
+}
 
 double Item_func_vec_distance::val_real()
 {

sql/sql_handler.cc
@@ -668,6 +668,7 @@ mysql_ha_fix_cond_and_key(SQL_HANDLER *handler,
 
       if (c_key->algorithm == HA_KEY_ALG_RTREE ||
           c_key->algorithm == HA_KEY_ALG_FULLTEXT ||
+          c_key->algorithm == HA_KEY_ALG_VECTOR ||
           (ha_rkey_mode != HA_READ_KEY_EXACT &&
            (table->key_info[handler->keyno].index_flags &
             (HA_READ_NEXT | HA_READ_PREV | HA_READ_RANGE)) == 0))

sql/sql_show.cc
@@ -2366,7 +2366,7 @@ int show_create_table_ex(THD *thd, TABLE_LIST *table_list,
   key_info= table->s->key_info;
   primary_key= share->primary_key;
 
-  for (uint i=0 ; i < share->keys ; i++,key_info++)
+  for (uint i=0 ; i < share->total_keys ; i++,key_info++)
   {
     if (key_info->flags & HA_INVISIBLE_KEY)
       continue;
@@ -2389,6 +2389,8 @@ int show_create_table_ex(THD *thd, TABLE_LIST *table_list,
       packet->append(STRING_WITH_LEN("FULLTEXT KEY "));
     else if (key_info->algorithm == HA_KEY_ALG_RTREE)
       packet->append(STRING_WITH_LEN("SPATIAL KEY "));
+    else if (key_info->algorithm == HA_KEY_ALG_VECTOR)
+      packet->append(STRING_WITH_LEN("VECTOR KEY "));
     else
       packet->append(STRING_WITH_LEN("KEY "));
 
@@ -2414,6 +2416,7 @@ int show_create_table_ex(THD *thd, TABLE_LIST *table_list,
         append_identifier(thd, packet, &key_part->field->field_name);
       if (key_part->field &&
           key_part->length != table->field[key_part->fieldnr-1]->key_length() &&
+          key_info->algorithm != HA_KEY_ALG_VECTOR &&
           key_info->algorithm != HA_KEY_ALG_RTREE &&
           key_info->algorithm != HA_KEY_ALG_FULLTEXT)
       {
@@ -7255,7 +7258,7 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
     bool need_column_checks= !get_schema_privileges_for_show(thd, tables,
                                                              TABLE_ACLS, false);
 
-    for (uint i=0 ; i < show_table->s->keys ; i++,key_info++)
+    for (uint i=0 ; i < show_table->s->total_keys ; i++,key_info++)
     {
       if ((key_info->flags & HA_INVISIBLE_KEY) &&
           !DBUG_IF("test_invisible_index"))
@@ -7311,6 +7314,7 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
                                    ? "D" : "A", 1, cs);
             table->field[8]->set_notnull();
           }
+          if (i < show_table->s->keys)
           {
             /*
               We have to use table key information to get the key statistics
@@ -7328,6 +7332,12 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
             const char *tmp= show_table->file->index_type(i);
             table->field[13]->store(tmp, strlen(tmp), cs);
           }
+          else
+          {
+            /* there are no others at the moment */
+            DBUG_ASSERT(key_info->algorithm == HA_KEY_ALG_VECTOR);
+            table->field[13]->store(STRING_WITH_LEN("VECTOR"), cs);
+          }
         }
         if (key_info->algorithm != HA_KEY_ALG_FULLTEXT &&
             (key_part->field &&
@@ -7341,7 +7351,7 @@ static int get_schema_stat_record(THD *thd, TABLE_LIST *tables, TABLE *table,
         uint flags= key_part->field ? key_part->field->flags : 0;
         const char *pos= flags & NOT_NULL_FLAG ? "" : "YES";
         table->field[12]->store(pos, strlen(pos), cs);
-        if (!show_table->s->keys_in_use.is_set(i))
+        if (i < show_table->s->keys && !show_table->s->keys_in_use.is_set(i))
           table->field[14]->store(STRING_WITH_LEN("disabled"), cs);
         DBUG_ASSERT(MY_TEST(key_info->flags & HA_USES_COMMENT) ==
                    (key_info->comment.length > 0));

sql/vector_mhnsw.cc
@@ -1,0 +1,209 @@
+/*
+   Copyright (c) 2024, MariaDB plc
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1335  USA
+*/
+
+#include <my_global.h>
+#include "vector_mhnsw.h"
+#include "field.h"
+#include "item.h"
+#include "sql_queue.h"
+#include <scope.h>
+
+const LEX_CSTRING mhnsw_hlindex_table={STRING_WITH_LEN("\
+  CREATE TABLE i (                                      \
+    src varbinary(255) not null,                        \
+    dst varbinary(255) not null,                        \
+    index (src))                                        \
+")};
+
+static void store_ref(TABLE *t, handler *h, uint n)
+{
+  t->hlindex->field[n]->store((char*)h->ref, h->ref_length, &my_charset_bin);
+}
+
+int mhnsw_insert(TABLE *table, KEY *keyinfo)
+{
+  TABLE *graph= table->hlindex;
+  MY_BITMAP *old_map= dbug_tmp_use_all_columns(table, &table->read_set);
+  Field *field= keyinfo->key_part->field;
+  String buf, *res= field->val_str(&buf);
+  handler *h= table->file;
+  int err= 0;
+  dbug_tmp_restore_column_map(&table->read_set, old_map);
+
+  /* metadata are checked on open */
+  DBUG_ASSERT(graph);
+  DBUG_ASSERT(keyinfo->algorithm == HA_KEY_ALG_VECTOR);
+  DBUG_ASSERT(keyinfo->usable_key_parts == 1);
+  DBUG_ASSERT(field->binary());
+  DBUG_ASSERT(field->cmp_type() == STRING_RESULT);
+  DBUG_ASSERT(res); // ER_INDEX_CANNOT_HAVE_NULL
+  DBUG_ASSERT(h->ref_length <= graph->field[0]->field_length);
+  DBUG_ASSERT(h->ref_length <= graph->field[1]->field_length);
+
+  if (res->length() == 0 || res->length() % 4)
+    return 1;
+
+  // let's do every node to every node
+  h->position(table->record[0]);
+  graph->field[0]->store(1);
+  store_ref(table, h, 0);
+
+  if (h->lookup_handler->ha_rnd_init(1))
+    return 1;
+  while (! ((err= h->lookup_handler->ha_rnd_next(h->lookup_buffer))))
+  {
+    h->lookup_handler->position(h->lookup_buffer);
+    if (graph->field[0]->cmp(h->lookup_handler->ref) == 0)
+      continue;
+    store_ref(table, h->lookup_handler, 1);
+    if ((err= graph->file->ha_write_row(graph->record[0])))
+      break;
+  }
+  h->lookup_handler->ha_rnd_end();
+
+  return err == HA_ERR_END_OF_FILE ? 0 : err;
+}
+
+static int cmp_float(void *, float *a, float *b)
+{
+  return *a < *b ? -1 : *a == *b ? 0 : 1;
+}
+
+struct Node
+{
+  float distance;
+  uchar ref[1000];
+};
+
+int mhnsw_first(TABLE *table, Item *dist, ulonglong limit)
+{
+  TABLE *graph= table->hlindex;
+  Queue<Node, float> todo, result;
+  Node *cur;
+  String *str, strbuf;
+  const size_t ref_length= table->file->ref_length;
+  const size_t element_size= ref_length + sizeof(float);
+  uchar *key= (uchar*)alloca(ref_length + 32);
+  Hash_set<Node> visited(PSI_INSTRUMENT_MEM, &my_charset_bin, limit,
+                         sizeof(float), ref_length, 0, 0, HASH_UNIQUE);
+  uint keylen;
+  int err= 0;
+
+  DBUG_ASSERT(graph);
+
+  if (todo.init(1000, 0, 0, cmp_float)) // XXX + autoextent
+    return HA_ERR_OUT_OF_MEM;
+
+  if (result.init(limit, 0, 1, cmp_float))
+    return HA_ERR_OUT_OF_MEM;
+
+  if ((err= graph->file->ha_index_init(0, 1)))
+    return err;
+
+  SCOPE_EXIT([graph](){ graph->file->ha_index_end(); });
+
+  // 1. read a start row
+  if ((err= graph->file->ha_index_last(graph->record[0])))
+    return err;
+
+  if (!(str= graph->field[0]->val_str(&strbuf)))
+    return HA_ERR_CRASHED;
+
+  DBUG_ASSERT(str->length() == ref_length);
+
+  cur= (Node*)table->in_use->alloc(element_size);
+  memcpy(cur->ref, str->ptr(), ref_length);
+
+  if ((err= table->file->ha_rnd_init(0)))
+    return err;
+
+  if ((err= table->file->ha_rnd_pos(table->record[0], cur->ref)))
+    return HA_ERR_CRASHED;
+
+  // 2. add it to the todo
+  cur->distance= dist->val_real();
+  if (dist->is_null())
+    return HA_ERR_END_OF_FILE;
+  todo.push(cur);
+  visited.insert(cur);
+
+  while (todo.elements())
+  {
+    // 3. pick the top node from the todo
+    cur= todo.pop();
+
+    // 4. add it to the result
+    if (result.is_full())
+    {
+      // 5. if not added, greedy search done
+      if (cur->distance > result.top()->distance)
+        break;
+      result.replace_top(cur);
+    }
+    else
+      result.push(cur);
+
+    float threshold= result.is_full() ? result.top()->distance : FLT_MAX;
+
+    // 6. add all its [yet unvisited] neighbours to the todo heap
+    keylen= graph->field[0]->get_key_image(key, ref_length, Field::itRAW);
+    if ((err= graph->file->ha_index_read_map(graph->record[0], key, 3,
+                                             HA_READ_KEY_EXACT)))
+      return HA_ERR_CRASHED;
+
+    do {
+      if (!(str= graph->field[1]->val_str(&strbuf)))
+        return HA_ERR_CRASHED;
+
+      if (visited.find(str->ptr(), ref_length))
+        continue;
+
+      if ((err= table->file->ha_rnd_pos(table->record[0], (uchar*)str->ptr())))
+        return HA_ERR_CRASHED;
+
+      float distance= dist->val_real();
+      if (distance > threshold)
+        continue;
+
+      cur= (Node*)table->in_use->alloc(element_size);
+      cur->distance= distance;
+      memcpy(cur->ref, str->ptr(), ref_length);
+      todo.push(cur);
+      visited.insert(cur);
+    } while (!graph->file->ha_index_next_same(graph->record[0], key, keylen));
+    // 7. goto 3
+  }
+
+  // 8. return results
+  Node **context= (Node**)table->in_use->alloc(sizeof(Node**)*result.elements()+1);
+  graph->context= context;
+
+  Node **ptr= context+result.elements();
+  *ptr= 0;
+  while (result.elements())
+    *--ptr= result.pop();
+
+  return mhnsw_next(table);
+}
+
+int mhnsw_next(TABLE *table)
+{
+  Node ***context= (Node***)&table->hlindex->context;
+  if (**context)
+    return table->file->ha_rnd_pos(table->record[0], (*(*context)++)->ref);
+  return HA_ERR_END_OF_FILE;
+}