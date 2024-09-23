MariaDB/server

VEC_Distance_Cosine()

sql/item_vectorfunc.h
@@ -85,6 +85,33 @@ public:
 };
 
 
+class Item_func_vec_distance_cosine: public Item_func_vec_distance_common
+{
+  double calc_distance(float *v1, float *v2, size_t v_len) override
+  {
+    double dotp=0, abs1=0, abs2=0;
+    for (size_t i= 0; i < v_len; i++, v1++, v2++)
+    {
+      abs1+= *v1 * *v1;
+      abs2+= *v2 * *v2;
+      dotp+= *v1 * *v2;
+    }
+    return 1 - dotp/sqrt(abs1*abs2);
+  }
+
+public:
+  Item_func_vec_distance_cosine(THD *thd, Item *a, Item *b)
+   :Item_func_vec_distance_common(thd, a, b) {}
+  LEX_CSTRING func_name_cstring() const override
+  {
+    static LEX_CSTRING name= { STRING_WITH_LEN("VEC_CosDistance") };
+    return name;
+  }
+  Item *do_get_copy(THD *thd) const override
+  { return get_item_copy<Item_func_vec_distance_cosine>(thd, this); }
+};
+
+
 class Item_func_vec_totext: public Item_str_ascii_checksum_func
 {
   bool check_arguments() const override

user0x1337/mariadb-server

sql/item_vectorfunc.h
@@ -85,6 +85,33 @@ public:
 };
 
 
+class Item_func_vec_distance_cosine: public Item_func_vec_distance_common
+{
+  double calc_distance(float *v1, float *v2, size_t v_len) override
+  {
+    double dotp=0, abs1=0, abs2=0;
+    for (size_t i= 0; i < v_len; i++, v1++, v2++)
+    {
+      abs1+= *v1 * *v1;
+      abs2+= *v2 * *v2;
+      dotp+= *v1 * *v2;
+    }
+    return 1 - dotp/sqrt(abs1*abs2);
+  }
+
+public:
+  Item_func_vec_distance_cosine(THD *thd, Item *a, Item *b)
+   :Item_func_vec_distance_common(thd, a, b) {}
+  LEX_CSTRING func_name_cstring() const override
+  {
+    static LEX_CSTRING name= { STRING_WITH_LEN("VEC_DISTANCE_COSINE") };
+    return name;
+  }
+  Item *do_get_copy(THD *thd) const override
+  { return get_item_copy<Item_func_vec_distance_cosine>(thd, this); }
+};
+
+
 class Item_func_vec_totext: public Item_str_ascii_checksum_func
 {
   bool check_arguments() const override